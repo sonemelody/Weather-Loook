6cca5381f0a32250ef584fcc07842a8359599195
KIM DOYEON <cat0806@naver.com>
2022-11-03 02:48:46 +0900

    calender menu + navigator drawer

--- app/src/main/java/com/mpl/weather/MainActivity.java
+++ app/src/main/java/com/mpl/weather/MainActivity.java
@@ -1,36 +1,94 @@
 package com.mpl.weather;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-
-import android.Manifest;
-import android.content.pm.PackageManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.BaseAdapter;
+import android.widget.CalendarView;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.widget.Toolbar;
 
 import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.models.SlideModel;
 import com.example.weather.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    ConstraintLayout weatherLayout;
+    TextView stateTextview;
+    TextView tempTextView;
+    ImageView weatherIcon;
+    CalendarView calenderView;
+    TextView date;
+
+    private String weatherState;
+    private String weatherTemp;
+    private WeatherData weatherData;
+
     FloatingActionButton fab, fab1, fab2;
     Animation fabOpen, fabClose, rotateForward, rotateBackward;
 
     boolean isOpen = false;
 
+    private DrawerLayout drawer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        weatherData = new WeatherData();
+        weatherLayout = (ConstraintLayout) findViewById(R.id.weatherLayout);
+        stateTextview = (TextView) findViewById(R.id.weatherState);
+        tempTextView = (TextView) findViewById(R.id.temperature);
+        weatherIcon = (ImageView) findViewById(R.id.weatherIcon);
+
+        new Thread() {
+            public void run() {
+                String[] temp = new String[2];
+                try {
+                    temp = getWeatherData(weatherData);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                weatherState = temp[0];
+                weatherTemp = temp[1];
+                Bundle bundle = new Bundle();
+                bundle.putString("State", weatherState);
+
+                Message msg = handler.obtainMessage();
+                msg.setData(bundle);
+                handler.sendMessage(msg);
+            }
+        }.start();
 
         ImageSlider imageSlider = findViewById(R.id.slider);
 
@@ -71,6 +129,99 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "take a photo", Toast.LENGTH_SHORT).show();
             }
         });
+
+        calenderView = findViewById(R.id.calendarView);
+        date = findViewById(R.id.date);
+
+        calenderView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String todayDate = (month+1) + "/" + dayOfMonth + "/" + year;
+                Log.d("date", todayDate);
+                date.setText(todayDate);
+            }
+        });
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if(savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new calendarFragment()).commit();
+            navigationView.setCheckedItem(R.id.calendarFragment);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.calendarFragment:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new calendarFragment()).commit();
+                break;
+            case R.id.galleryFragment:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new galleryFragment()).commit();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+
+    }
+
+    private String[] getWeatherData(WeatherData weatherData) throws JSONException, IOException {
+        final String[] dataString = new String[2];
+        String[] temp = weatherData.lookUpWeather();
+        dataString[0] = temp[0];
+        dataString[1] = temp[1];
+        return dataString;
+    }
+
+    Handler handler = new Handler() {
+        public void handleMessage(Message msg) {
+            Bundle bundle = msg.getData();
+            String msgString = bundle.getString("state");
+            changeWeatherStateBackground(weatherState);
+            tempTextView.setText(weatherTemp + "도");
+        }
+    };
+
+    private void changeWeatherStateBackground(String state) {
+        if (state.equals("맑음")) {
+            weatherLayout.setBackgroundColor(getApplicationContext().getResources().getColor(R.color.sunny));
+            stateTextview.setText(getApplicationContext().getResources().getString(R.string.sunny));
+            weatherIcon.setImageResource(R.drawable.sunny);
+        } else if (state.equals("흐림")) {
+            weatherLayout.setBackgroundColor(getApplicationContext().getResources().getColor(R.color.dark));
+            stateTextview.setText(getApplicationContext().getResources().getString(R.string.dark));
+            weatherIcon.setImageResource(R.drawable.dark);
+        } else if (state.equals("구름많음")) {
+            weatherLayout.setBackgroundColor(getApplicationContext().getResources().getColor(R.color.cloudy));
+            stateTextview.setText(getApplicationContext().getResources().getString(R.string.cloudy));
+            weatherIcon.setImageResource(R.drawable.cloudy);
+        } else {
+            weatherLayout.setBackgroundColor(getApplicationContext().getResources().getColor(R.color.rain));
+            stateTextview.setText(getApplicationContext().getResources().getString(R.string.rain));
+            weatherIcon.setImageResource(R.drawable.rain);
+        }
     }
 
     private void animateFab() {
---
+++ app/src/main/java/com/mpl/weather/WeatherData.java
@@ -0,0 +1,125 @@
+package com.mpl.weather;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class WeatherData extends Thread {
+
+    private String nx = "55";
+    private String ny = "127";
+    private String baseDate = "";
+    private String baseTime = "0630";
+    private String type = "json";
+    private String weatherState = "";
+    private Double weatherTemp = 0.0;
+    private String pageNo = "1";
+    private String numOfRows = "1000";
+
+    public String[] lookUpWeather() throws IOException, JSONException {
+        long now = System.currentTimeMillis();
+        Date date = new Date(now);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
+        baseDate = dateFormat.format(date);
+
+        String apiUrl = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst";
+        String serviceKey = "bblped1fj1rwoLuY06oOCNnw%2B%2FnW97U1cZXfubIkK1YpznRPiOsi7dHb%2F%2FarMS1Buk7nLZ917PG%2Bc8bFdz%2F%2F%2Fw%3D%3D";
+        StringBuilder urlBuilder = new StringBuilder(apiUrl);
+        urlBuilder.append("?" + URLEncoder.encode("ServiceKey","UTF-8") + "="+serviceKey);
+        urlBuilder.append("&" + URLEncoder.encode("nx","UTF-8") + "=" + URLEncoder.encode(nx, "UTF-8")); //경도
+        urlBuilder.append("&" + URLEncoder.encode("ny","UTF-8") + "=" + URLEncoder.encode(ny, "UTF-8")); //위도
+        urlBuilder.append("&" + URLEncoder.encode("base_date","UTF-8") + "=" + URLEncoder.encode(baseDate, "UTF-8")); /* 조회하고싶은 날짜*/
+        urlBuilder.append("&" + URLEncoder.encode("base_time","UTF-8") + "=" + URLEncoder.encode(baseTime, "UTF-8")); /* 조회하고싶은 시간 AM 02시부터 3시간 단위 */
+        urlBuilder.append("&" + URLEncoder.encode("dataType","UTF-8") + "=" + URLEncoder.encode(type, "UTF-8"));
+        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode(numOfRows, "UTF-8"));
+        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode(pageNo, "UTF-8"));	/* 타입 *//* 타입 */
+        /* 타입 */
+
+        /*
+         * GET방식으로 전송해서 파라미터 받아오기
+         */
+        URL url = new URL(urlBuilder.toString());
+
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-type", "application/json");
+        //System.out.println("Response code: " + conn.getResponseCode());
+
+        BufferedReader rd;
+        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        } else {
+            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+        }
+
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = rd.readLine()) != null) {
+            sb.append(line);
+        }
+
+        rd.close();
+        conn.disconnect();
+        String result= sb.toString();
+
+        //=======이 밑에 부터는 json에서 데이터 파싱해 오는 부분이다=====//
+
+        // response 키를 가지고 데이터를 파싱
+        JSONObject jsonObj_1 = new JSONObject(result);
+        String response = jsonObj_1.getString("response");
+
+        // response 로 부터 body 찾기
+        JSONObject jsonObj_2 = new JSONObject(response);
+        String body = jsonObj_2.getString("body");
+
+        // body 로 부터 items 찾기
+        JSONObject jsonObj_3 = new JSONObject(body);
+        String items = jsonObj_3.getString("items");
+
+        // items로 부터 itemlist 를 받기
+        JSONObject jsonObj_4 = new JSONObject(items);
+        JSONArray jsonArray = jsonObj_4.getJSONArray("item");
+
+        for(int i=0;i<jsonArray.length();i++){
+            jsonObj_4 = jsonArray.getJSONObject(i);
+            String fcstValue = jsonObj_4.getString("fcstValue");
+            String category = jsonObj_4.getString("category");
+            String fcstTime = jsonObj_4.getString("fcstTime");
+
+            if (fcstTime.equals("0800")) {
+                if(category.equals("T3H") || category.equals("T1H")){
+                    weatherTemp = Double.valueOf(fcstValue);
+                }
+
+                if(category.equals("SKY")){
+                    if (fcstValue.equals("1")) {
+                        weatherState = "맑음";
+                    } else if (fcstValue.equals("3")) {
+                        weatherState = "구름많음";
+                    } else if (fcstValue.equals("4")) {
+                        weatherState = "흐림";
+                    } else {
+                        weatherState = fcstValue;
+                    }
+                }
+
+                if(category.equals("PTY")){
+                    if (Integer.parseInt(fcstValue) > 0) {
+                        weatherState = "비";
+                    }
+                }
+            }
+        }
+
+        return new String[] {weatherState, weatherTemp.toString()};
+    }
+}
